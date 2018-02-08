a85a1eb2c46abbe1b1640e2d462c806252535c07
sujian <sujiansoft@163.com>
2018-02-08 15:11:20 +0800

    123

--- entry.sh
+++ entry.sh
@@ -5,8 +5,10 @@ echo ">>>>>>>>>>>>>>>>>>>start set hosts <<<<<<<<<<<"
 cp /etc/hosts /etc/hosts.temp
 sed -i "s/.*$(hostname)/${ip} $(hostname)/" /etc/hosts.temp
 cat /etc/hosts.temp > /etc/hosts
-cat /etc/hosts
+
+echo /etc/hosts
 echo ">>>>>>>>>>>>>>>>>>>>end set hosts >>>>>>>>>>>"
 
 echo "cleaning and reset  finish..........."
-java-jar  usr/app/app.jar
+
+java -jar  usr/app/app.jar
