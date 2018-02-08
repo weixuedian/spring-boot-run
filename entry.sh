2160c2b31477c191818866a20e9ed8dbe4357b31
weixuedian <112606652@qq.com>
2018-02-08 11:43:46 +0800

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -1,10 +1,12 @@
-echo >>>>>>>>>>>>'date'<<<<<<<<<<<<<<<<<<<<<
+#!/usr/bin/env bash
+echo "********************`date`********************"
 
 echo ">>>>>>>>>>>>>>>>>>>start set hosts <<<<<<<<<<<"
-    cp /etc/hosts /etc/hosts.temp
-    sed -i "s/.*$(hostname)/${ip} $(hostname)/" /etc/hosts.temp
-    cat /etc/hosts.temp > /etc/hosts
-    cat /etc/hosts
+cp /etc/hosts /etc/hosts.temp
+sed -i "s/.*$(hostname)/${ip} $(hostname)/" /etc/hosts.temp
+cat /etc/hosts.temp > /etc/hosts
+cat /etc/hosts
 echo ">>>>>>>>>>>>>>>>>>>>end set hosts >>>>>>>>>>>"
-    
+
+echo "cleaning and reset  finish..........."
 java-jar  usr/app/app.jar
